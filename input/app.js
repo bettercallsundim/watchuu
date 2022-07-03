8df4b32729fbcb4cba5c12357040cf0c4c8b18df
meandme <sunmaxboom2@gmail.coml>
2022-07-03 13:40:45 +0600

    ok

--- input/app.js
+++ input/app.js
@@ -1,10 +1,35 @@
-var swiper = new Swiper(".mySwiper", {
-   pagination: {
-     el: ".swiper-pagination",
-   },
-   slidesPerView : 3,
- });
-
+if(window.screen.width>768){
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView : 3,
+  });
+}else{
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    slidesPerView : 2,
+  });
+}
+window.onresize=()=>{
+  if(window.screen.width>768){
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      slidesPerView : 3,
+    });
+  }else{
+    var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      slidesPerView : 2,
+    });
+  }
+}
  const $=document.querySelector.bind(document);
  const $$=document.querySelectorAll.bind(document);
  $('.menu').onclick=()=>{
